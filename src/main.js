3795499352db897469f0e9c96db0d05d1c5c53f7
jeissonR115 <jeissonfeliper@gmail.com>
2024-04-22 01:43:04 -0500

    prueba error git hub

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import { renderizarMenu, renderizarLogo } from "/src/header.js";
 import { renderizarFooter } from "./footer.js";
 document.addEventListener("DOMContentLoaded", () => {
     const logo = document.querySelector('.logo');
-
+    const nav = document.querySelector(".nav");
+    const footer = document.querySelector(".footer");
+    renderizarHead()
+    if(nav)nav.appendChild(renderizarMenu());
     if(logo)logo.appendChild(renderizarLogo());
-
+    if(footer)footer.appendChild(renderizarFooter());
 });
 
